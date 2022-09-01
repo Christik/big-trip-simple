ec3fe4632246e6479ea7efb6ef0886d7d3d08511
Christina Yanovich <christik.lucid@gmail.com>
2022-09-01 13:58:28 +0300

    Добавляет перечисления для фильтров

---
+++ src/enum/filter-label.js
@@ -0,0 +1,6 @@
+import Enum from './enum.js';
+
+export default class Filterlabel extends Enum {
+  static EVERYTHING = 'Everything';
+  static FUTURE = 'Future';
+}
---
+++ src/enum/filter-predicate.js
@@ -0,0 +1,12 @@
+/** @typedef {import('../adapter/point-adapter').default} PointAdapter */
+
+import Enum from './enum.js';
+
+export default class FilterPredicate extends Enum {
+  static EVERYTHING = () => true;
+
+  /**
+   * @param {PointAdapter} point
+   */
+  static FUTURE = (point) => (Date.parse(point.endDate) > Date.now());
+}
